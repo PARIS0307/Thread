2b04f0713dce77d9ad324c95c1eaf209a702f1da
Paris <AgusPrasetyoer0307@proton.me>
2026-02-27 07:06:20 -0800

    Add Executor(with Callable)

--- src/Executor.java
+++ src/Executor.java
@@ -1,19 +1,18 @@
 import java.lang.Thread;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Executor {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService executor = Executors.newFixedThreadPool(3);
-
-        Runnable task = () -> {
-            System.out.println("Running: " + java.lang.Thread.currentThread().getName());
+        Callable<Integer> task = () -> {
+            Thread.sleep(1000);
+            return 100;
         };
 
-        for (int i = 0; i < 5; i++) {
-            executor.execute(task);
-        }
+        Future<Integer> future = executor.submit(task);
+        System.out.println("Doing other work...");
+        Integer result = future.get();
+        System.out.println("Result: " + result);
         executor.shutdown();
     }
-
 }
